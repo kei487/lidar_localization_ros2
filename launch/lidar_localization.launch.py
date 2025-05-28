dedcd8a73a3b8dc19c068812f1bccb92312f3b6e
Keitaro Nakamrua <numerugon487@gmail.com>
2025-05-28 15:26:01 +0900

    change static tf map to odom

--- launch/lidar_localization.launch.py
+++ launch/lidar_localization.launch.py
@@ -31,7 +31,7 @@ def generate_launch_description():
         name='odom_tf',
         package='tf2_ros',
         executable='static_transform_publisher',
-        arguments=['0','0','0','0','0','0','1','odom','base_link']
+        arguments=['0','0','0','0','0','0','1','map','odom']
         )
 
     localization_param_dir = launch.substitutions.LaunchConfiguration(
